17b45870b76bfc7c796f650dd7fdf4b825879370
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-06-16 02:50:48 +0000

    4 chnages

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-// Created by: Lucas Tyman
+// Copyright (c) 2022 Marshall All rights reserved
+//
+// Created by: Marshall
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Check service worker.
+ * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Template/sw.js", {
-    scope: "/ICS2O-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-Unit6-01-HTML/sw.js", {
+    scope: "/ICS2O-Unit6-01-HTML/",
   })
 }
 
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculate() {
+  // input
+  const fahrenheit = parseFloat(
+    document.getElementById("fahrenheit-celsius").value
+  )
+
+  // process
+  const celsius = ((fahrenheit - 32) * 5) / 9
+
+  // output
+  document.getElementById("conversion").innerHTML =
+    fahrenheit + "°F converted to Celsius is " + celsius.toFixed(2) + " ℃"
 }
